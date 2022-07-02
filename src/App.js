93691353c790a8e83fcb1de64511187c6a7be5b6
farias_77 <gabrielfalves2003@gmail.com>
2022-07-02 12:06:47 -0300

    finalizando página de login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './components/Login.js';
+import SignIn from './components/SignIn.js';
 import './assets/reset.css';
 import './assets/styles.css';
 import { useState } from 'react';
 import SignUp from './components/SignUp.js';
+import Main from './components/Main.js'
 
 export default function App(){
 
-    const [token, setToken] = useState('vazio');
-
+    const [token, setToken] = useState('');
+    
     return (
         <BrowserRouter>
             <Routes>     
-                <Route path='/' element={<Login setToken={setToken}/>} />
+                <Route path='/' element={<SignIn setToken={setToken}/>} />
                 <Route path='/sign-up' element={<SignUp />} />
+                <Route path='/main' element={<Main token={token} />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export default function Main({ token }){
+
+
+
+
+    return ( 
+        <Container>
+
+
+
+        </Container>
+    )
+}
+
+const Container = styled.div`
+
+
+
+`;
